4019741a4a936412a8938b12cd7f4f76abfa266e
vZori <109729130+vZoriteYspX@users.noreply.github.com>
2026-06-22 22:51:47 +0800

    Update Reload.cs
    Tested inheritance
    
    Added the first offensive moves

--- Reload.cs
+++ Reload.cs
@@ -4,30 +4,45 @@ namespace Game
 {
   public class MainProgram
   {
-  static void Main(string[] args)
+    static void Main(string[] args)
     {
-      DefensiveMove teleport = new DefensiveMove();
-      Console.WriteLine(teleport.teleportMove);
-      Console.WriteLine(teleport.defCharge);
-      }
+      DefensiveMove move001 = new();
+      Console.WriteLine($"Player 1 used: {move001.teleportMove} for {move001.charge} charge(s)!");
     }
-
-  public class DefensiveMove
+  }
+  public class Charges
   {
-    public int defCharge;
+    public int charge;
+    public float halfCharge;
+  }
+  public class DefensiveMove : Charges
+  {
     public string teleportMove;
     public string vanishMove;
     public string shieldMove;
     public string barrierMove;
     public string nukeBarrierMove;
+
     public DefensiveMove()
     {
-    defCharge = 0;
-     teleportMove = "Teleport";
-     vanishMove = "Vanish";
-     shieldMove = "Shield";
-     barrierMove = "Barrier";
-     nukeBarrierMove = "Nuke Barrier";
+      charge = 0;
+      teleportMove = "Teleport";
+      vanishMove = "Vanish";
+      shieldMove = "Shield";
+      barrierMove = "Barrier";
+      nukeBarrierMove = "Nuke Barrier";
+    }
+  }
+  public class HalfChargeMove : Charges
+  {
+    public string bangMove;
+    public string DMove;
+
+    public HalfChargeMove()
+    {
+      halfCharge = 0.5F;
+      bangMove = "Bang";
+      DMove = "D";
     }
   }
 }
