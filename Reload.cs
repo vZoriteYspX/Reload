0a9f4d38a7ce96eed6e6e50faa67235fe817f585
vZori <109729130+vZoriteYspX@users.noreply.github.com>
2026-06-25 23:46:18 +0800

    Update Reload.cs
    [Update]
    Client-side
    - Turn 1 is now playable
    
    Server-side
    - Started using switch statements

--- Reload.cs
+++ Reload.cs
@@ -2,47 +2,69 @@ using System;
 
 namespace Game
 {
-  public class MainProgram
-  {
-    static void Main(string[] args)
+    public class MainProgram
     {
-      DefensiveMove move001 = new();
-      Console.WriteLine($"Player 1 used: {move001.teleportMove} for {move001.charge} charge(s)!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What will be your next move?");
+            string userInput1 = Console.ReadLine();
+            Player1 turn = new(userInput1);
+        }
     }
-  }
-  public class Charges
-  {
-    public int charge;
-    public float halfCharge;
-  }
-  public class DefensiveMove : Charges
-  {
-    public string teleportMove;
-    public string vanishMove;
-    public string shieldMove;
-    public string barrierMove;
-    public string nukeBarrierMove;
 
-    public DefensiveMove()
+    public class Charges
     {
-      charge = 0;
-      teleportMove = "Teleport";
-      vanishMove = "Vanish";
-      shieldMove = "Shield";
-      barrierMove = "Barrier";
-      nukeBarrierMove = "Nuke Barrier";
+        public int defCharge = 0;
+        public float halfCharge = 0.5F;
     }
-  }
-  public class HalfChargeMove : Charges
-  {
-    public string bangMove;
-    public string DMove;
 
-    public HalfChargeMove()
+    public class Player1 : Charges
     {
-      halfCharge = 0.5F;
-      bangMove = "Bang";
-      DMove = "D";
+        public Player1(string userInput1)
+        {
+            MoveUsed p1Move001 = new();
+            switch(userInput1)
+            {
+                case "Teleport":
+                {
+                    Console.WriteLine($"Player 1 used: {p1Move001.teleportMove} for {p1Move001.defCharge} charge(s)!");
+                    break;
+                }
+
+                case "Vanish":
+                {
+                    Console.WriteLine($"Player 1 used: {p1Move001.vanishMove} for {p1Move001.defCharge} charge(s)!");
+                    break;
+                }
+
+                case "Shield":
+                {
+                    Console.WriteLine($"Player 1 used: {p1Move001.shieldMove} for {p1Move001.defCharge} charge(s)!");
+                    break;
+                }
+
+                case "Barrier":
+                {
+                    Console.WriteLine($"Player 1 used: {p1Move001.barrierMove} for {p1Move001.defCharge} charge(s)!");
+                    break;
+                }
+
+                case "Nuke Barrier":
+                {
+                    Console.WriteLine($"Player 1 used: {p1Move001.nukeBarrierMove} for {p1Move001.defCharge} charge(s)!");
+                    break;
+                }
+            }
+        }
     }
-  }
+
+    public class MoveUsed : Charges
+    {
+        public string teleportMove = "Teleport";
+        public string vanishMove = "Vanish";
+        public string shieldMove = "Shield";
+        public string barrierMove = "Barrier";
+        public string nukeBarrierMove = "Nuke Barrier";
+        public MoveUsed() { }
+    }
 }
