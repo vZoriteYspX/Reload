eb393e88b170fb8ad664b0ee0783adf3c404198b
vZori <109729130+vZoriteYspX@users.noreply.github.com>
2026-06-28 23:53:18 +0800

    Update Reload.cs
    [Update]
    Client-side
    - Added the first offensive move options
    
    Server-side
    - Started using if-else statements

--- Reload.cs
+++ Reload.cs
@@ -2,69 +2,103 @@ using System;
 
 namespace Game
 {
+
+    
     public class MainProgram
     {
         static void Main(string[] args)
         {
             Console.WriteLine("What will be your next move?");
-            string userInput1 = Console.ReadLine();
-            Player1 turn = new(userInput1);
+            string userInput1 = Console.ReadLine().ToLower();
+            Player1 turnUsed = new(userInput1);
         }
     }
 
+
     public class Charges
     {
-        public int defCharge = 0;
-        public float halfCharge = 0.5F;
+        public float p1Charge = 1;
+        public const int defCharge = 0;
+        public const float halfCharge = 0.5F;
+        public const int oneCharge = 1;
+        public const int twoCharge = 2;
+        public const int threeCharge = 3;
+        public const int fourCharge = 4;
     }
+
 
     public class Player1 : Charges
     {
         public Player1(string userInput1)
         {
-            MoveUsed p1Move001 = new();
             switch(userInput1)
             {
-                case "Teleport":
+                case "teleport":
                 {
-                    Console.WriteLine($"Player 1 used: {p1Move001.teleportMove} for {p1Move001.defCharge} charge(s)!");
+                    Console.WriteLine($"Player 1 used {MoveUsed.teleportMove} for {defCharge} charge(s)!");
                     break;
                 }
 
-                case "Vanish":
+                case "vanish":
                 {
-                    Console.WriteLine($"Player 1 used: {p1Move001.vanishMove} for {p1Move001.defCharge} charge(s)!");
+                    Console.WriteLine($"Player 1 used {MoveUsed.vanishMove} for {defCharge} charge(s)!");
                     break;
                 }
 
-                case "Shield":
+                case "shield":
                 {
-                    Console.WriteLine($"Player 1 used: {p1Move001.shieldMove} for {p1Move001.defCharge} charge(s)!");
+                    Console.WriteLine($"Player 1 used {MoveUsed.shieldMove} for {defCharge} charge(s)!");
                     break;
                 }
 
-                case "Barrier":
+                case "barrier":
                 {
-                    Console.WriteLine($"Player 1 used: {p1Move001.barrierMove} for {p1Move001.defCharge} charge(s)!");
+                    Console.WriteLine($"Player 1 used {MoveUsed.barrierMove} for {defCharge} charge(s)!");
                     break;
                 }
 
-                case "Nuke Barrier":
+                case "nuke barrier":
                 {
-                    Console.WriteLine($"Player 1 used: {p1Move001.nukeBarrierMove} for {p1Move001.defCharge} charge(s)!");
+                    Console.WriteLine($"Player 1 used {MoveUsed.nukeBarrierMove} for {defCharge} charge(s)!");
                     break;
                 }
+
+                case "bang" when p1Charge >= halfCharge:
+                {
+                    if (p1Charge % 1 == 0)
+                    {
+                        p1Charge = (int)(p1Charge - halfCharge);
+                    }
+                    
+                    else
+                    {
+                        p1Charge -= halfCharge;    
+                    }
+                    Console.WriteLine($"Player 1 used {MoveUsed.bangMove} for {halfCharge} charge(s)!");
+                    break;
+                }
+
+                case "bang" when p1Charge < halfCharge:
+                {
+                    Console.WriteLine("You currently do not have enough charges to perform this move.");
+                    break;
+                }
+
+                        
             }
         }
     }
 
+
     public class MoveUsed : Charges
     {
-        public string teleportMove = "Teleport";
-        public string vanishMove = "Vanish";
-        public string shieldMove = "Shield";
-        public string barrierMove = "Barrier";
-        public string nukeBarrierMove = "Nuke Barrier";
+        public const string teleportMove = "teleport";
+        public const string vanishMove = "vanish";
+        public const string shieldMove = "shield";
+        public const string barrierMove = "barrier";
+        public const string nukeBarrierMove = "nuke barrier";
+        public const string bangMove = "bang";
+        public const string dMove = "d";
         public MoveUsed() { }
     }
 }
