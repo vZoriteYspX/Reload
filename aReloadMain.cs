d031798f1d1612262534ad0858177bf00ce59be7
vZori <109729130+vZoriteYspX@users.noreply.github.com>
2026-08-14 19:04:27 +0800

    Create aReloadMain.cs

---
+++ aReloadMain.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+
+namespace ReloadGame;
+
+public class MainProgram
+{
+    //  MAIN METHOD
+    static void Main(string[] args)
+    {   
+        new Start();  //  BEGINS THE START INPUT
+    }
+}
+
+
+public class Start
+{
+    public Start()
+    {
+        while (true)
+        {
+            char startInput;
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Start game? [Y / N]");
+
+
+            //  CATCHES INPUTS THAT ARE NOT Y OR N
+            try
+            {
+                startInput = Convert.ToChar((Console.ReadLine() ?? "").ToUpper());
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("An invalid input was entered. Please input \"Y/y\" or \"N/n\" to start the game.");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"{e.Message}");
+                continue;
+            }
+
+
+            //  STARTS GAME IF Y WAS INPUTTED
+            if (startInput == 'Y')
+            {
+                Console.WriteLine("Have fun! You are now proceeding to the game's setup.");
+                Console.WriteLine("----------------------------");
+
+                try
+                {
+                    Gameplay gameStart = new(true);  //  STARTS THE GAME PROPER
+                }
+                    
+                catch
+                {
+                    Console.WriteLine("----------------------------");
+                    Console.WriteLine("Oops, something went wrong with the game.");
+                    Console.WriteLine("----------------------------");   
+                }
+
+                break;
+            }
+
+
+            //  ENDS CODE IF N WAS INPUTTED
+            else if (startInput == 'N')
+            {   
+                Console.WriteLine("All right, see you again next time!");
+                break;
+            }
+                
+            else
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("An invalid input was entered. Please input \"Y/y\" or \"N/n\" to start the game.");
+                Console.WriteLine("----------------------------");
+            }
+        }
+    }
+}
